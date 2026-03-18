9e653ac21bd2ca7c8bdd71e2e65aac98cd4c0e46
stiven <mstivenvelezc@gmail.com>
2026-03-18 14:59:57 -0500

    ultimos retoques

--- js/script.js
+++ js/script.js
@@ -56,10 +56,10 @@ function generate1(){
 }
 function generate(){
   let menu=document.getElementById("menu");
-  if(menu.style.display=="none"){
-    menu.style.display="block";
-  }else{
+  if(menu.style.display=="block"){
     menu.style.display="none";
+  }else{
+    menu.style.display="block";
   }
 }
 
